740706c388cbf6adb7808c63e71f4b5d9836e8ef
cristobal-z <cristobalzaldivar0101@gmail.com>
2023-07-24 23:03:26 -0600

    se modifico el campo de texto total en modal refacciones

--- Views/Template/Modals/modalRefacciones.php
+++ Views/Template/Modals/modalRefacciones.php
@@ -154,7 +154,7 @@
 
               <label for="usu_total">Total de la venta</label>
 
-              <input type="text" class="form-control" id="usu_total" placeholder="$" name="usu_total">
+              <input type="text" class="form-control valid validNumber" id="usu_total" placeholder="$" name="usu_total">
 
             </div>
 
